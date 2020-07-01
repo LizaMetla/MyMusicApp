30db9126d64047b0a769fafd4d698ecb20dd929e
liza.metla <liza.metla@gmail.com>
2020-07-01 17:15:19 +0300

    initial commit

---
+++ MyMusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusicApp
+//
+//  Created by Елизавета Метла on 7/1/20.
+//  Copyright © 2020 Елизавета Метла. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
